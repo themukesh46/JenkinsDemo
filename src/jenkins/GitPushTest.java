036894e4387ba19e5f5f359d9d79eb677565bd17
themukesh46 <ssmukesh46@gmail.com>
2024-08-04 17:18:54 +0530

    added second line

--- src/jenkins/GitPushTest.java
+++ src/jenkins/GitPushTest.java
@@ -4,8 +4,8 @@ public class GitPushTest {
 
     public static void main (String args []){
 
-        System.out.print("this is first git oush test.");
-
+        System.out.print("this is first git push test.");
+        System.out.print("this is second git oush test.");
 
 
     }
