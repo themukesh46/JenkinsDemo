7d8d147918ed5a4929f9a1959899ccb91544d20e
themukesh46 <ssmukesh46@gmail.com>
2024-08-04 17:30:37 +0530

    pushed code to git

--- src/jenkins/GitPushTest.java
+++ src/jenkins/GitPushTest.java
@@ -5,7 +5,7 @@ public class GitPushTest {
     public static void main (String args []){
 
         System.out.print("this is first git push test.");
-        System.out.print("this is second git oush test.");
+
 
 
     }
